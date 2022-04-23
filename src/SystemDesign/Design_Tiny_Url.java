20b69aed42be186d25c7613287b9d87fcbfa37c8
Avnish Kumar <avnishgolu927@gmail.com>
2022-04-23 09:44:43 +0530

    Tiny Url

---
+++ src/SystemDesign/Design_Tiny_Url.java
@@ -0,0 +1,10 @@
+package SystemDesign;
+
+
+/// Here we are given a long string of the website link our task is to design the tiny url from it.
+
+public class Design_Tiny_Url {
+    public static void main(String[] args) {
+        System.out.println("Hii Welocme ");
+    }
+}
